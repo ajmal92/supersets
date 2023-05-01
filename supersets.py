c91956a2fa341c77c8b066eae97dbfd44588fcdc
ajmal92 <ajmaltanur@gmail.com>
2023-05-01 10:48:32 +0530

    Update supersets.py

--- supersets.py
+++ supersets.py
@@ -1,11 +1,27 @@
 import requests
 import json
+import argparse
+import subprocess
+import tempfile
 
-base_url = "https://analytics.ci.ccctechcenter.org/api/v1/"
+
+
+
+def options():
+    parser = argparse.ArgumentParser(description='Streamsets CI/CD script')
+    parser.add_argument('-base_url', default='base_url', required=True)
+    parser.add_argument('-username', help='Username', required=True)
+    parser.add_argument('-password', help='password', required=True)
+    args = parser.parse_args()
+    return args
+
+args = options()
+  
+base_url = args.base_url
 
 payload = json.dumps({
-  "password": "admin",
-  "username": "admin@superset.com",
+  "password": args.password,
+  "username": args.username,
   "refresh": True,
   "provider": "db"
 })
@@ -19,24 +35,25 @@ access_token = response.json()['access_token']
 headers['Authorization'] = f'Bearer {access_token}'
 response = requests.request("GET", f"{base_url}/security/csrf_token", headers=headers)
 
-csrf_token = response.json()['result'] nb 3
-print(access_token)
-print(csrf_token)
+csrf_token = response.json()['result']
 
-DASHBOARD_FILE_PATH = "C:\GIT\superset\config\sindhu_test.zip"
-
+base_path = "config"
+temp_dir = tempfile.gettempdir()
+zip_dashboard_path = os.path.join(temp_dir, 'supersets')
+subprocess.run(['mkdir','-p', zip_dashboard_path)
 headers = {
   'Content-Type': 'application/json',
   'X-CSRFToken' :csrf_token
 }
 
-with open(DASHBOARD_FILE_PATH, 'rb') as f:
-    dashboard_zip_content = f.read()
-    
-files={
-        'file': ('dashboard.zip', dashboard_zip_content)
-    }
-resp = requests.request("POST", f"{base_url}/dashboard/import", headers=headers, files=files)
+for folder in os.listdir(f"{config}/"):
+  dashboard_file_path = os.path.join(zip_dashboard_path, folder) + ".zip"
+  subprocess.run(['zip', '-r', dashboard_file_path, folder], cwd=zip_dashboard_path)
+  with open(dashboard_file_path, 'rb') as f:
+      dashboard_zip_content = f.read()
 
-print(resp.status_code)
-print(resp.json())
+  files={
+          'file': ('dashboard.zip', dashboard_zip_content)
+      }
+  resp = requests.request("POST", f"{base_url}/dashboard/import", headers=headers, files=files)
+  assert resp.status_code == 201
